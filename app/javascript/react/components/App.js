ef9e5be741dfe6838e57cce8d85690a0c6782d13
Brianna Kincart <briannakincart@gmail.com>
2019-10-08 16:44:24 -0400

    new version with hooks and correct pattern as of fall 2019

---
+++ app/javascript/react/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import TasksIndexContainer from "./TasksIndexContainer"
+
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/tasks" component={TasksIndexContainer}/>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ app/javascript/react/components/TasksIndexContainer.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+
+const TasksIndexContainer = (props) => {
+  debugger;
+  const [tasks, setTasks] = useState([])
+  debugger;
+
+  useEffect(() => {
+    fetch("/api/v1/tasks.json")
+    .then(response => response.json())
+    .then(taskBody => {
+      debugger;
+      setTasks(taskBody)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }, [])
+
+  let tasktiles = tasks.map((task) => {
+    return(
+      <li key={task.id}>
+        {task.name}
+      </li>
+    )
+  })
+
+  return(
+    <div>
+      <h3> Task Index Container </h3>
+      {tasktiles}
+    </div>
+  )
+}
+
+export default TasksIndexContainer
